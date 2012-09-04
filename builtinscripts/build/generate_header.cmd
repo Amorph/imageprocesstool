007d9ed11d470479889962e840ec7defefa41cd6
Amorph <amorph.dev@gmail.com>
2012-09-04 20:10:03 +0300

    Fix projects rebuilding that depends on builtinscripts

--- builtinscripts/build/generate_header.cmd
+++ builtinscripts/build/generate_header.cmd
@@ -4,6 +4,11 @@
 cd %1
 cd ..
 cd obj
+if not exist "rebuild_header" goto exit
+
 if exist "../include/scripts.h" del "../include/scripts.h"
 if not exist "../include" md "../include"
 @for /r %%i in (*.h) do type %%i >> "../include/scripts.h"
+
+del rebuild_header
+:exit
